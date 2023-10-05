7208a7cab2df8744ac406660d7ac80f67cf93ab9
unknown <sebastian.zamora-u@labinfo.is.escuelaing.edu.co>
2023-10-05 18:44:23 -0500

    Trabajo en clase

--- src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/model/Employee.java
@@ -10,11 +10,11 @@ import jakarta.persistence.Table;
 public class Employee {
     @Id
     @Column(name = "EMPLOYEE_ID")
-    private String employee_id;
-    @Column(name = "FIRTS_NAME")
-    private String firts_name;
+    private String id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
     @Column(name = "LAST_NAME")
-    private String last_name;
+    private String lastName;
 
     @Column(name = "ROLE")
     private String role;
@@ -24,35 +24,36 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(String employee_id, String firts_name,String last_name,String role,float salary) {
-        this.employee_id = employee_id;
-        this.firts_name = firts_name;
-        this.last_name = last_name;
+    public Employee(String id, String firstName, String lastName, String role, float salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
         this.salary = salary;
     }
 
-    public String getEmployee_id() {
-        return employee_id;
+    public String getId() {
+        return id;
     }
 
-    public void setEmployee_id(String employee_id) {
-        this.employee_id = employee_id;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getFirts_name() {
-        return firts_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirts_name(String firts_name) {
-        this.firts_name = firts_name;
-    }
-    public String getLast_name() {
-        return last_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getRole() {
@@ -72,54 +73,14 @@ public class Employee {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((employee_id == null) ? 0 : employee_id.hashCode());
-        result = prime * result + ((firts_name == null) ? 0 : firts_name.hashCode());
-        result = prime * result + ((last_name == null) ? 0 : last_name.hashCode());
-        result = prime * result + ((role == null) ? 0 : role.hashCode());
-        result = prime * result + ((Float.toString(salary) == null) ? 0 : Float.toString(salary).hashCode());
-
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Employee other = (Employee) obj;
-        if (employee_id == null) {
-            if (other.employee_id != null)
-                return false;
-        } else if (!employee_id.equals(other.employee_id))
-            return false;
-
-        if (firts_name == null) {
-            if (other.firts_name != null)
-                return false;
-        } else if (!firts_name.equals(other.firts_name))
-            return false;
-
-        if (last_name == null) {
-            if (other.last_name != null)
-                return false;
-        } else if (!last_name.equals(other.last_name))
-            return false;
-        if (role == null) {
-            if (other.role != null)
-                return false;
-        } else return role.equals(other.role);
-
-        return true;
-    }
-
-    @Override
     public String toString() {
-        return "Employee [id = " + employee_id + ", name = " + firts_name + last_name +", role = "+ role + ", salary = "+ salary+"]";
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role='" + role + '\'' +
+                ", salary=" + salary +
+                '}';
     }
+
 }
--- src/main/java/co/edu/escuelaing/cvds/lab7/service/EmployeeService.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/service/EmployeeService.java
@@ -1,6 +1,5 @@
 package co.edu.escuelaing.cvds.lab7.service;
 
-import co.edu.escuelaing.cvds.lab7.model.Configuration;
 import co.edu.escuelaing.cvds.lab7.model.Employee;
 import co.edu.escuelaing.cvds.lab7.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import java.util.List;
 
 @Service
 public class EmployeeService {
+
     private final EmployeeRepository employeeRepository;
 
     @Autowired
@@ -17,16 +17,34 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
+
     public Employee addEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
-    //public Employee getEmployee(String employee_id) {
-    //    return employeeRepository.findByEmployee_id(employee_id).get(1);
-    //}
+
+    public Employee getEmployee(String employee_id) {
+        //return employeeRepository.findByEmployee_id(employee_id).get(1);
+        return employeeRepository.findById(employee_id);
+    }
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    public Employee updateEmployee(Employee employee) {
+        if (employeeRepository.findById(employee.getId()) == null) {
+            return employeeRepository.save(employee);
+        }
+
+        return null;
+    }
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    //public String getPremio() {
+        //return configurationRepository.findByPropiedad("premio").get(0).getValor();
+    //}
 
 
 
